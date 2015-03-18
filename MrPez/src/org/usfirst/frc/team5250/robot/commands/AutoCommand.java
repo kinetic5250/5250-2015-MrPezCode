7299a90404fd89ea82c82903b1fed7daa8110952
googolydox <googolydox@gmail.com>
2015-03-18 15:31:52 -0700

    Autonomous

--- MrPez/src/org/usfirst/frc/team5250/robot/commands/AutoCommand.java
+++ MrPez/src/org/usfirst/frc/team5250/robot/commands/AutoCommand.java
@@ -1,7 +1,23 @@
 package org.usfirst.frc.team5250.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
+
 public class AutoCommand extends CommandBase {
 	public AutoCommand() {
 		requires(driveTrain);
+	}
+	
+	public void initialize() {
+		System.out.println("AutoCommand");
+	}
+	
+	public void execute() {
+		driveTrain.tankDrive(0.5, 0.5);
+		Timer.delay(5);
+		driveTrain.tankDrive(0, 0);
+	}
+	
+	public boolean isFinished() {
+		return true;
 	}
 }
